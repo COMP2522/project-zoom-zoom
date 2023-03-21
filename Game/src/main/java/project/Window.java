25ef6c6106cb5d11e54cf3d814ea4167a88a738f
jamesnll <langillejames11@gmail.com>
2023-03-21 15:01:50 -0700

    Removed old code from class. Added case for car mod menu in draw() method. Updated main method variables.

--- Game/src/main/java/project/Window.java
+++ Game/src/main/java/project/Window.java
@@ -20,8 +20,8 @@ public class Window extends PApplet {
   Timer timer2;
   MainMenu mainMenu;
   ControlMenu controlMenu;
+  CarModMenu carModMenu;
   ArrayList<Sprite> sprites;
-  ArrayList<Sprite> enemies;
   Player player1;
   Player player2;
   Controls playerControls;
@@ -29,9 +29,7 @@ public class Window extends PApplet {
   int[] player1Keys = {38, 40, 37, 39};
   // Default player2 controls (WASD)
   int[] player2Keys = {87, 83, 65, 68};
-  int numEnemies = 1;
   int minSize = 10;
-  int maxSize = 40;
   public static boolean audio = true;
   private int check = 1;
 
@@ -39,13 +37,12 @@ public class Window extends PApplet {
    * 0. Main menu
    * 1. 1 player game
    * 2. 2 player game
+   * 3. Control Menu
+   * 4. Car Mod Menu
    * more values will come when menus are implemented.
    * TODO
-   *  car mod menu
    *  track menu
-   *  control menu
    * car mod and track menus should happen before game starts
-   * control menu button in main menu
    */
   int menu = 0;
 
@@ -57,17 +54,9 @@ public class Window extends PApplet {
   }
 
   /**
-   * Called once at the beginning of the program.
-   * Initializes all objects.
-   */
-  public void setup() {
-  }
-
-  /**
    * Initializes all sprites needed for a one player game.
    */
   public void init1Player() {
-    enemies = new ArrayList<Sprite>();
     sprites = new ArrayList<Sprite>();
 
     player1 = new Player(
@@ -86,25 +75,12 @@ public class Window extends PApplet {
         new Color(0, 255, 0),
         this);
     playerControls = new Controls(this, player1, player2, player1Keys, player2Keys);
-
-    for (int i = 0; i < numEnemies; i++) {
-      enemies.add(new Enemy(
-          new PVector(random(0, this.width), random(0, this.height)),
-          new PVector(random(-1, 1), random(-1, 1)),
-          random(minSize, maxSize),
-          random(0, 2),
-          new Color(255, 0, 0),
-          this
-      ));
-    }
-    sprites.addAll(enemies);
     sprites.add(player1);
   }
   /**
    * Initializes all sprites needed for a two player game.
    */
   public void init2Player() {
-    enemies = new ArrayList<Sprite>();
     sprites = new ArrayList<Sprite>();
 
     player1 = new Player(
@@ -123,18 +99,6 @@ public class Window extends PApplet {
         new Color(0, 255, 255),
         this);
     playerControls = new Controls(this, player1, player2, player1Keys, player2Keys);
-
-    for (int i = 0; i < numEnemies; i++) {
-      enemies.add(new Enemy(
-          new PVector(random(0, this.width), random(0, this.height)),
-          new PVector(random(-1, 1), random(-1, 1)),
-          random(minSize, maxSize),
-          random(0, 2),
-          new Color(255, 0, 0),
-          this
-      ));
-    }
-    sprites.addAll(enemies);
     sprites.add(player1);
     sprites.add(player2);
   }
@@ -193,21 +157,6 @@ public class Window extends PApplet {
           sprite.update();
           sprite.draw();
         }
-        ArrayList<Sprite> toRemove = new ArrayList<Sprite>();
-        for (Sprite enemy : enemies) {
-          if (Collidable.collided(player1, enemy)) {
-            toRemove.add(enemy);
-          }
-
-        }
-        for (Sprite enemy : toRemove) {
-          // TODO: implement compareTo and equals to make this work
-          if (enemy.compareTo(player1) > 0 || enemy.equals(player1)) {
-            enemies.remove(enemy);
-            sprites.remove(enemy);
-          } else if (enemy.compareTo(player1) < 0) {
-          }
-        }
         break;
       }
       case 2 -> { // 2 Player game
@@ -221,21 +170,6 @@ public class Window extends PApplet {
           sprite.update();
           sprite.draw();
         }
-        ArrayList<Sprite> toRemove = new ArrayList<Sprite>();
-        for (Sprite enemy : enemies) {
-          if (Collidable.collided(player1, enemy)) {
-            toRemove.add(enemy);
-          }
-
-        }
-        for (Sprite enemy : toRemove) {
-          // TODO: implement compareTo and equals to make this work
-          if (enemy.compareTo(player1) > 0 || enemy.equals(player1)) {
-            enemies.remove(enemy);
-            sprites.remove(enemy);
-          } else if (enemy.compareTo(player1) < 0) {
-          }
-        }
         break;
       }
       case 3 -> {
@@ -244,6 +178,11 @@ public class Window extends PApplet {
         controlMenu.setup();
         controlMenu.draw();
         break;
+      }
+      case 4 -> {
+        carModMenu = CarModMenu.getInstance(this);
+        carModMenu.setup();
+        carModMenu.draw();
       }
       default -> {
         break;
@@ -258,9 +197,9 @@ public class Window extends PApplet {
    * @param passedArgs arguments from command line
    */
   public static void main(String[] passedArgs) {
-    String[] appletArgs = new String[]{"eatBubbles"};
-    Window eatBubbles = new Window();
-    PApplet.runSketch(appletArgs, eatBubbles);
+    String[] appletArgs = new String[]{"Zoom Zoom"};
+    Window zooomZoom = new Window();
+    PApplet.runSketch(appletArgs, zooomZoom);
     // Run background music
   }
 }
