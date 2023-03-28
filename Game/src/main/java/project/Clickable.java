9945dc7bcdc6caeb30dda7c45f717c444f275169
jamesnll <langillejames11@gmail.com>
2023-03-28 14:46:48 -0700

    Created interface to check if button was clicked

---
+++ Game/src/main/java/project/Clickable.java
@@ -0,0 +1,14 @@
+package project;
+
+import processing.core.PApplet;
+
+/**
+ * Clickable, interface for buttons to handle mouse click events.
+ *
+ * @author James Langille
+ */
+public interface Clickable {
+
+  void click();
+
+}
