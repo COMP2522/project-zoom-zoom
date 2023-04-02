95ac6869e3c8a6edb33c7db9a236848162b413a5
jamesnll <langillejames11@gmail.com>
2023-04-02 16:41:54 -0700

    Moved code relating to images from MainMenu to MainMenuImages

--- Game/src/main/java/project/MainMenu.java
+++ Game/src/main/java/project/MainMenu.java
@@ -1,27 +1,13 @@
 package project;
 
-import processing.core.PConstants;
-import processing.core.PImage;
-import processing.core.PVector;
-
 /**
  * MainMenu, menu that appears once the game has been launched.
  *
  * @author James Langille
  */
 public class MainMenu implements Drawable {
-  // Images
-  private PImage bgImage;
-  private PImage gameTitle;
-  private PImage onePlayerImage;
-  private PImage twoPlayerImage;
-  private PImage controlsImage;
-  private PImage quitImage;
-  // Other data
   protected static GameManager window;
   private static MainMenu instance;
-  private boolean showTitle = true;
-  private static final int clock = 5;
 
   /**
    * Main menu, private constructor to create a singleton of the class.
@@ -46,35 +32,11 @@ public class MainMenu implements Drawable {
   }
 
   /**
-   * showTitle, shows the game title every half second.
-   */
-  private void showTitle() {
-    if (window.frameCount % clock == 0) {
-      // Sets to opposite boolean expression every second
-      showTitle = !showTitle;
-    }
-    if (showTitle) {
-      window.image(gameTitle, window.displayWidth / 3 - 40, window.displayHeight / 4);
-    }
-  }
-
-  /**
    * setup, Sets up all objects needed for the main menu.
    */
   public void setup() {
-    window.imageMode(PConstants.CORNER);
-    window.rectMode(PConstants.CORNER);
-    // Set up buttons
     MainMenuButtons.setupButtons();
-
-    // Set up images
-    bgImage = window.loadImage("Game/images/BGImage.png");
-    gameTitle = window.loadImage("Game/images/zoomZoom.png");
-    onePlayerImage = window.loadImage("Game/images/1Player.png");
-    twoPlayerImage = window.loadImage("Game/images/2Player.png");
-    controlsImage = window.loadImage("Game/images/Controls.png");
-    quitImage = window.loadImage("Game/images/Quit.png");
-//    scoreboard = window.loadImage("Game/images/Ranking.jpg");
+    MainMenuImages.setupImages();
   }
 
   /**
@@ -82,18 +44,9 @@ public class MainMenu implements Drawable {
    */
   @Override
   public void draw() {
-    // Draw images
-    window.image(bgImage, 0, 0, window.displayWidth, window.displayHeight);
+    MainMenuImages.drawBackground();
     MainMenuButtons.drawButtons();
-//    window.image(scoreboard, 0, 0, (float) (window.displayWidth / 20) + 100, 825);
-    this.showTitle();
-//    window.image(onePlayerImage, onePlayer.getPosition().x + 5, onePlayer.getPosition().y + 10);
-//
-//    window.image(twoPlayerImage, twoPlayer.getPosition().x + 5, twoPlayer.getPosition().y + 10);
-    // Draw controls button and image
-//    window.image(controlsImage, controls.getPosition().x + 5, controls.getPosition().y + 10);
-//    // Draw quit button and image
-//    window.image(quitImage, quit.getPosition().x + 60, quit.getPosition().y + 10);
+    MainMenuImages.drawImages();
   }
 
 }
