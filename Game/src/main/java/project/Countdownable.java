db0eab11be853e1c8d9a830e539740bbf6229d93
jamesnll <langillejames11@gmail.com>
2023-04-02 20:58:08 -0700

    Created interface to add a 3-second countdown before the race begins

---
+++ Game/src/main/java/project/Countdownable.java
@@ -0,0 +1,11 @@
+package project;
+
+/**
+ * Countdownable, interface that adds a 3-second delay before the race begins.
+ *
+ * @author James Langille
+ */
+public interface Countdownable {
+
+  void countdown();
+}
