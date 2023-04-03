ec982702542bd49d38fc1a5585e4e06cd24020f7
chino0522 <baekjinho0522@gmail.com>
2023-04-02 22:45:58 -0700

    Bgm class conflict has been resolved

--- Game/src/main/java/project/Bgm.java
+++ Game/src/main/java/project/Bgm.java
@@ -1,26 +1,38 @@
 package project;
 
+import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
 
 /**
  * The BGM class is responsible for handling background music in the program.
  */
-public class BGM {
+public class Bgm {
 
   /**
    * The clip variable holds the audio clip that is currently playing.
    */
-  private static Clip clip;
+  private Clip clip;
+
+  private static Bgm instance;
+
+  private Bgm() {}
+
+  public static Bgm getInstance() {
+    if (instance == null) {
+      instance = new Bgm();
+    }
+    return instance;
+  }
 
   /**
    * The getBGM method loads the audio file and plays the background music.
+   *
    * @param check - a boolean value indicating whether the music should be played or not
    * @throws Exception if the audio file cannot be loaded or if there is an error playing the audio
    */
-  public static void getBGM(boolean check) {
+  public void getBGM(boolean check) {
     try {
       File audioFile = new File("tokyodrift.wav");
       AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
@@ -36,9 +48,10 @@ public class BGM {
 
   /**
    * The stopBGM method stops the background music from playing.
+   *
    * @param check - a boolean value indicating whether the music should be stopped or not
    */
-  public static void stopBGM(boolean check) {
+  public void stopBGM(boolean check) {
     if (!check) {
       clip.close();
     }
