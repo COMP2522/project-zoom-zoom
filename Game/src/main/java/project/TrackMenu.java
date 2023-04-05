627ebbf697972b18fae4baf5a5a026220d64cc24
chino0522 <baekjinho0522@gmail.com>
2023-04-05 11:52:19 -0700

    Finished Refactoring TrackMenu

--- Game/src/main/java/project/TrackMenu.java
+++ Game/src/main/java/project/TrackMenu.java
@@ -3,21 +3,21 @@ package project;
 import processing.core.PImage;
 import processing.core.PVector;
 
-import java.awt.*;
-
 /**
- * The TrackMenu class displays the track selection menu for the game. It provides the user with the option
- * to select one of the three available tracks. This class is a singleton class, meaning there can only be one
- * instance of this class throughout the game.
- * The class contains three buttons, each representing a track. When a button is clicked, the game menu state is
- * updated to start the game on the selected track.
+ * The TrackMenu class displays the track selection menu for the game.
+ * It provides the user with the option to select one of the three
+ * available tracks. This class is a singleton class, meaning
+ * there can only be one instance of this class throughout the game.
  */
 public class TrackMenu {
 
+  /**
+   * The TrackManager object that creates and manages the tracks.
+   */
   private TrackManager trackManager;
 
   /**
-   * The GameManager object that creates and manages the game window
+   * The GameManager object that creates and manages the game window.
    */
   private final GameManager window;
 
@@ -42,34 +42,47 @@ public class TrackMenu {
   private Button track3;
 
   /**
-   * This is a private boolean variable that determines whether or not to show the title of the track menu
-   * depends on the frame of the window.
+   * This is a private boolean variable that determines whether or not
+   * to show the title of the track menu depends on the frame of the window.
    */
   private boolean showTitle = true;
 
   /**
-   * This is a private static final integer variable representing the clock rate of the title animation.
+   * This is a private static final integer variable representing the clock
+   * rate of the title animation.
    */
   private static final int clock = 5;
 
-  /**
-   * These are private instance variables of the PImage class representing the background image, title
-   * image, and font images for each track button.
-   */
-  private PImage bg, title, track1Font, track2Font, track3Font;
+  private float halfDisplayWidth;
 
   /**
-   * This is a private constructor for the TrackMenu class that takes a GameManager object as a parameter.
+   * These are private instance variables of the PImage
+   * class representing the background image, title
+   * image, and font images for each track button.
+   */
+  private PImage bg;
+  private PImage title;
+  private PImage track1Font;
+  private PImage track2Font;
+  private PImage track3Font;
+
+  /**
+   * This is a private constructor for the TrackMenu class that takes
+   * a GameManager object as a parameter.
+   *
    * @param gameManager The window for the current window.
    */
   private TrackMenu(GameManager gameManager) {
     this.window = gameManager;
     trackManager = window.getTrackManager();
+    halfDisplayWidth = (float) (window.displayWidth / 2);
   }
 
   /**
-   * This is a public static method that returns an instance of the TrackMenu class. If an instance of the
-   * class does not already exist, a new one is created.
+   * This is a public static method that returns an instance of the
+   * TrackMenu class. If an instance of the class does not already
+   * exist, a new one is created.
+   *
    * @param window The game manager for the current game.
    * @return The singleton instance of the TrackMenu class.
    */
@@ -81,48 +94,83 @@ public class TrackMenu {
   }
 
   /**
-   * This is a public method that sets up the track menu. It loads the background image, title image, and
-   * font images for each track button, creates the track buttons, and sets their positions and colors.
+   * Gets the image from the path.
+   *
+   * @param name the name of the image
+   * @return PImage the path of the image
    */
-  public void setUp() {
-    bg = window.loadImage("Game/images/BGImage2.png");
-    title = window.loadImage("Game/images/Select Track.png");
-    track1Font = window.loadImage("Game/images/Track1.png");
-//    track2Font = window.loadImage("Game/images/Track2.png");
-//    track3Font = window.loadImage("Game/images/Track3.png");
-    window.textSize(40);
-    track1 = new Button(new PVector((float) (window.displayWidth / 2) - 525, 490), 300,
-        80, "", new Color(104, 52, 235), window);
-    track2 = new Button(new PVector((float) (window.displayWidth / 2) - 125, 490), 300,
-        80, "", new Color(64,64,64), window);
-    track3 = new Button(new PVector((float) (window.displayWidth / 2) + 325, 490), 300,
-        80, "", new Color(64,64,64), window);
+  private PImage getImage(String name) {
+    return window.loadImage("Game/images/" + name);
   }
 
   /**
-   * This is a public method that draws the track menu. It sets the background color,
-   * draws the background image, toggles the title visibility based on the clock rate,
-   * draws the title image, updates and draws each track button, and changes the menu
-   * to the race screen if the first track button is clicked.
+   * This is a public method that sets up the track menu.
+   * It loads the background image, title image, and
+   * font images for each track button, creates the track buttons,
+   * and sets their positions and colors.
+   */
+  public void setUp() {
+    bg = getImage("BGImage2.png");
+    title = getImage("Select Track.png");
+    track1Font = getImage("Track1.png");
+    track2Font = getImage("Track2.png");
+    track3Font = getImage("Track3.png");
+    window.textSize(40);
+    track1 = new Button(new PVector(halfDisplayWidth - 525, 490), 300,
+        80, "", Button.PURPLE, window);
+    track2 = new Button(new PVector(halfDisplayWidth - 125, 490), 300,
+        80, "", Button.PURPLE, window);
+    track3 = new Button(new PVector(halfDisplayWidth + 325, 490), 300,
+        80, "", Button.PURPLE, window);
+  }
+
+  /**
+   * This is a public method that draws the track menu.
+   * It draws the background image, title image, and
+   * font images for each track button, and draws the track buttons.
    */
   public void draw() {
     window.background(64, 64, 64);
     window.image(bg, 0, 0, window.displayWidth, window.displayHeight);
+    drawTitle();
+    drawFont();
+    checkButtonClick();
+  }
+
+  /**
+   * This is a private method that draws the title image of the track menu.
+   * It animates the title image by changing the showTitle boolean variable
+   * every 5 frames.
+   */
+  private void drawTitle() {
     if (window.frameCount % clock == 0) {
       showTitle = !showTitle;
     }
     if (showTitle) {
-      window.image(title, (float) (window.displayWidth / 2) - 200,window.displayHeight/4);
+      window.image(title, halfDisplayWidth - 200, window.displayHeight / 4);
     }
+  }
+
+  /**
+   * This is a private method that draws the font images for each track button.
+   * It draws the font images for each track button, and draws the track buttons.
+   */
+  private void drawFont() {
     track1.click();
     track1.draw();
-    window.image(track1Font, (float) (window.displayWidth / 2) - 500, 500);
+    window.image(track1Font, halfDisplayWidth - 500, 500);
     track2.click();
     track2.draw();
-    //window.image(track2Font, (float) (window.displayWidth / 2 - 500 / 2), 500);
+    window.image(track2Font, halfDisplayWidth - 100, 500);
     track3.click();
     track3.draw();
-    //window.image(track3Font, (float) (window.displayWidth / 2) - 250, 500);
+    window.image(track3Font, halfDisplayWidth + 350, 500);
+  }
+
+  /**
+   * This is a private method that checks if the user has clicked on one of the track buttons.
+   */
+  private void checkButtonClick() {
     if (track1.isLeftClicked()) {
       trackManager.clearTrack();
       trackManager.initTrack("Track 1");
